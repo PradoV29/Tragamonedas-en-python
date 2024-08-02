ec10905359760654bd06e37fd99752fbed2133d7
PradoV29 <nuclearbop08@gmail.com>
2024-08-01 19:42:53 -0500

    agregue nuevas funciones como apuesta y opciones

--- Tragamonedas.py
+++ Tragamonedas.py
@@ -1,55 +1,95 @@
 import random
-import pygame, sys
 
 class Tragamonedas():
     def __init__(self, creditos, bonus):
         self.creditos = creditos
         self.bonus = bonus
-        self.opciones = {'naranja':10,'campana':15,'bar':50, 'bar/bar':100, 
-                         'manzana':5, '77':40, 'cereza':2, 'melon':20,
-                         'sandia': 25, 'cereza':2, 'manzana': 5, 'cereza':2,
-                         'naranja':10, 'campana':15, 'cereza':2, '77':40, 
-                         'manzana':5, 'cereza':2, 'melon':20, 'estrellas':30,
-                         'cereza':2, 'manzana':5, 'cereza':2}
+        self.opciones = [
+            {'naranja': 10},
+            {'campana': 15},
+            {'bar': 50},
+            {'bar/bar': 100},
+            {'manzana': 5},
+            {'77': 40},
+            {'cereza': 2},
+            {'melon': 20},
+            {'sandia': 25},
+            {'cereza': 2},
+            {'manzana': 5},
+            {'cereza': 2},
+            {'naranja': 10},
+            {'campana': 15},
+            {'cereza': 2},
+            {'77': 40},
+            {'manzana': 5},
+            {'cereza': 2},
+            {'melon': 20},
+            {'estrellas': 30},
+            {'cereza': 2},
+            {'manzana': 5},
+            {'cereza': 2}
+        ]
 
-    def insertar_creditos(self):
-        self.creditos += 1
+    def insertar_monedas(self):
+        while True:
+            monedas = int(input("Ingrese el numero de monedas: "))
+            if 0 < monedas <= 999:
+                self.creditos += monedas
+                print(f"Ingresastes {monedas} monedas lo cual se cambia a {self.creditos} creditos")
+                break
+            else:
+                print("El numero maximo de monedas que se puede es de 999")
+
     def bonus(self):
         self.bonus += 1
-    def aleatorio(self):
-       clave_aleatoria = random.choice(list(self.opciones.keys()))
-       valor = self.opciones[clave_aleatoria]
-       return valor
 
-pygame.init()
-size = (1500, 1000)
-blanco = 255, 255, 255
-cafe = 128, 64, 0
-rojo = (255, 0, 0)
-fuente = pygame.font.Font(None, 74) 
-def mostrar_texto(ventana, texto, color, posicion):
-    superficie_texto = fuente.render(texto, True, color)
-    ventana.blit(superficie_texto, posicion)
+    def tablero(self):
+        print("***TABLERO***\n")
+        print("a) Bar/Bar")
+        print("b) Bar")
+        print("c) 77")
+        print("d) Estrellas")
+        print("e) Sandia")
+        print("f) Melon")
+        print("g) Campana")
+        print("h) Naranja")
+        print("i) Manzana")
+        print("j) Cereza")
+    
+    def mostrar_opciones(self):
+        print("1. TABLERO")
+        print("2. INSERTAR MONEDAS")
+        print("3. APUESTA")
+        print("0. SALIR")
+    
+    def apuesta(self):
+        nu_apuestas = int(input("Cuantas apuestas desea realizar: "))
+        while nu_apuestas:
+            if self.creditos > 0:
+                creditos_apostar = int(input("Cuantos creditso desea apostar: "))
+                self.creditos -= creditos_apostar
+            elif self.creditos <= 0:
+                print("No tiene saldo para hacer la apuesta, ¿desea insertar? (Si/No)")
+                insertar = input().lower()
+                if insertar == 'si':
+                    tragamonedas.insertar_monedas()
+                else:
+                    print("Opcion incorrecta")
+            else: 
+                girar = random.choice(self.opciones)
+                print(girar)
+                break
 
-ventana = pygame.display.set_mode(size)
+tragamonedas = Tragamonedas(creditos=0, bonus=0)
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            sys.exit()
-    ventana.fill(blanco)
-
-    for x in range (100, 700, 100):
-        pygame.draw.rect(ventana, cafe, (x, 10, 90, 90))
-        for y in range (10, 700, 100):
-            pygame.draw.rect(ventana, cafe, (100, y, 90, 90))
-
-    for x in range (200, 600, 100):
-        pygame.draw.rect(ventana, cafe, (x, 610, 90, 90))
-        for y in range (10, 700, 100):
-            pygame.draw.rect(ventana, cafe, (600, y, 90, 90)) 
-
-    pygame.draw.rect(ventana, rojo, (100, 10, 90, 90), 5)
-
-    mostrar_texto(ventana, 'BONUS', rojo, (100, 100))
-
-    pygame.display.flip()
+    print("OPCIONES")
+    print(tragamonedas.mostrar_opciones())
+    opc = int(input("Ingrese una opcion: "))
+    if opc == 1:
+        print(tragamonedas.tablero())
+    elif opc == 2:
+        print(tragamonedas.insertar_monedas())
+    elif opc == 3:
+        print(tragamonedas.apuesta())
+    elif opc == 0:
+        break
